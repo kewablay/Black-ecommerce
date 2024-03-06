09ed96079d59ae7bbc5e223f5907760786ce2f61
kewablay <kojokewajunior123@gmail.com>
2024-03-06 23:59:23 +0000

    Added Edit crypto and add crypto modals

--- src/pages/CryptoWallets/components/CryptoListItem.jsx
+++ src/pages/CryptoWallets/components/CryptoListItem.jsx
@@ -1,15 +1,19 @@
 import { DeleteIcon, EditIcon } from "assets/icons/svgIcons";
+import EditCryptoModal from "components/modals/EditCryptoModal";
+import useCustomModal from "hooks/useCustomModal";
 import React from "react";
 
 function CryptoListItem({ crypto }) {
+  const { openModal, closeModal, ModalComponent } = useCustomModal();
   return (
     <div className="grid items-center grid-cols-12 gap-5 p-5 bg-white rounded-md shadow-sm flex-between text-300">
+      {ModalComponent()}
       <p className="col-span-3">{crypto.name}</p>
       <p className="col-span-6">{crypto.address}</p>
       <div className="flex items-center col-span-3 gap-5">
         <button
           className="flex items-center gap-1"
-          //   onClick={() => setEditing(true)}
+          onClick={() => openModal(<EditCryptoModal />, "customModal")}
         >
           <span>
             <EditIcon />
